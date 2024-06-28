bc54c8823d4db9c3717d2c8f549d58ba20240dcc
limbanga <95570833+limbanga@users.noreply.github.com>
2024-06-29 00:25:44 +0700

    add product model

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model): 
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    abstract=True
+
+class Product(BaseModel):
+  name = models.CharField(max_length=255)
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  description = models.TextField()
--- project/project/settings/__init__.py
+++ project/project/settings/__init__.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-print(BASE_DIR)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -38,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'app',
 ]
 
 MIDDLEWARE = [
