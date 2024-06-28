8ae0a51bede709f2973de4c2d10b21c9b47f0694
limbanga <95570833+limbanga@users.noreply.github.com>
2024-06-29 00:30:11 +0700

    add owner

--- project/app/models.py
+++ project/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class BaseModel(models.Model): 
   created = models.DateTimeField(auto_now_add=True)
@@ -11,3 +12,6 @@ class Product(BaseModel):
   name = models.CharField(max_length=255)
   price = models.DecimalField(max_digits=5, decimal_places=2)
   description = models.TextField()
+  # foreign keys
+  # TODO: Remove null=True and default=None later
+  owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
