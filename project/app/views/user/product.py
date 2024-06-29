11c15a212c5241fb2d61718e7e9b3d8796b84f84
limbanga <95570833+limbanga@users.noreply.github.com>
2024-06-29 14:44:24 +0700

    save product

--- project/app/views/user/product.py
+++ project/app/views/user/product.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from app.forms import ProductForm
+
 PREFIX = 'user/products'
 
 def index(request):
     return render(request, f'{PREFIX}/index.html')
 
+@login_required
 def create(request):
     form = ProductForm()
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save() # save the form to the database
+            
+            product = form.save(commit=False) # commit=False means don't save yet
+            product.owner = request.user
+            product.save()
+            return redirect('index')
 
     return render(request, f'{PREFIX}/edit.html', {'form': form})
