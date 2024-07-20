c925d16e8a194f1f57323aa584e0fec2cf7b6560
limbanga <95570833+limbanga@users.noreply.github.com>
2024-07-21 02:33:58 +0700

    add image

--- project/app/models.py
+++ project/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+
 class BaseModel(models.Model): 
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
@@ -13,12 +14,16 @@ class Product(BaseModel):
   name = models.CharField(max_length=255)
   price = models.DecimalField(max_digits=10, decimal_places=2)
   description = RichTextField()
+  image = models.ImageField(upload_to='products', null=True, default=None)
   # foreign keys
   # TODO: Remove null=True and default=None later
   owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
   category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, default=None)
+
   def __str__(self)->str:
     return self.name
+
+
 
 
 class Category(BaseModel):
--- project/app/urls.py
+++ project/app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home, auth, error_404_view
 from .views.user import product, category, shop
 
@@ -40,3 +42,4 @@ urlpatterns = [
 ]
 
 handler404 = error_404_view
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
