2d7a968bd86979098a784cbf63e59faef6858d87
limbanga <95570833+limbanga@users.noreply.github.com>
2024-07-22 20:58:09 +0700

    update login form

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Product, Category, Order, OrderLine, Review, UserInformation
+from .models import *
 
-# Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Category)
@@ -9,11 +8,8 @@ admin.site.register(Order)
 admin.site.register(OrderLine)
 admin.site.register(Review)
 admin.site.register(UserInformation)
+admin.site.register(ProductImage)
+
 
 admin.site.site_header = "Bast Admin"
-# Compare this snippet from project/app/views.py:
-# from django.shortcuts import render, redirect
-# from django.http import HttpResponse, HttpRequest
-# from django.contrib.auth.models import User
-# from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
-# from django.contrib.auth.decorators import login_required
+
